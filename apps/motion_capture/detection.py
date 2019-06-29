8d55c9a85bfc47136a39f7b2186e84e9d8a7308b
Holden.B <holden.bab@outlook.com>
2019-06-28 23:20:58 -0500

    Refactors to detector code

--- apps/motion_capture/detection.py
+++ apps/motion_capture/detection.py
@@ -1,9 +1,18 @@
 import os
+import sys
+import subprocess
 import glob
 import random
 import statistics		
 import time
 from pathlib import Path
+
+# System path to the top level project directory
+sys.path.append(os.path.join(os.path.dirname(os.path.realpath(__file__)), '../..'))
+
+# Local imports
+import ffmpeg_py.ffmpeg.calls as calls
+import ffmpeg_py.utils as utils
 
 
 '''
@@ -35,39 +44,55 @@ FILE_FORMATS = [
     'AVI', 'QT', 'MXF', 'MOV', 'MTS',
     'M2TS','MPEG', 'VOB', 'IFO']
 
-#basic parameters
-before_s = 2.5              #start copying video N seconds before motion is triggered  
-after_s = 2                 #end copying video N seconds after motion has ended
-min_copy_break_s = 5.9      #don't stop copying if next motion trigger sooner than this
-ignore_start_s = 2          #seconds don't search for motion in beginning of input file
-ignore_end_s = 2            #seconds don't search for motion at end of input file
-generate_output_file = True #set to False if you only want to read logs
-delete_input_file = False   #DANGEROUS, use only if you have COPIES of input files
-
-#out file parameters
-out_prefix = ''             #begin output filename with this
-out_from_in_start = 2       #substring of input file name. Large number copies entire filename
-out_from_in_end = 0         #substring of end of input file name
-out_random_letters = 1      #amount of random letters to add to output files. Same letters for all.
-out_counter_digits = 2      #least digits to counter. 2 means _01, _02, _03 etc
-out_lower_case = True       #force lower case filename
-out_delimiter = ''          #e.g. '-' to make filename more readable
-
-#cmd window log
-print_scores = False        #whether to print frame info (including scene scores)
-ffmpeg_loglevel = 31        #see https://ffmpeg.org/ffmpeg.html#Generic-options
-
-#advanced filter parameters
-step_len_f = 20             #compare every n frame
-min_threshold_score = 0.0095#default threshold. a score above indicates motion
-test_duration_s = 7         #seek for a ('motionless'ish') segment this long. threshold automatically adjusts up if necessary (and possible)
-max_threshold_score = 0.04
-segments_smooth = 0         #assign median score from n segments before and after to smooth out scores
-segments_to_start = 2       #this many segments in a row above threshold triggers motion start
-segments_to_end = 10        #this many segments in a row below threshold triggers motion end
-
-
+# TODO Refactor this code so the detector is a lot simpler to understand. Also add
+# functionality to take multiple input files as per original implementation.
+# Possibly implement parallelism with multi file processing.
 class Detector:
+    """ FFmpeg Detect & Copy Motion (FDCM) dector object.
+    FFmpeg Detect & Copy Motion (FDCM) automatically detects motion in video files.
+    Each video motion event is saved to a new, separate video file. It works on most
+    videos with a fixed camera angle and (almost) motionless background.
+
+    FDCM is optimized for speed. A 1080p video should be processed 3-5x faster than
+    the duration of the input file.
+
+    The extracted files are lossless copies from the input file bitstream.
+    """
+    #basic parameters
+    before_s = 2.5              #start copying video N seconds before motion is triggered  
+    after_s = 2                 #end copying video N seconds after motion has ended
+    min_copy_break_s = 5.9      #don't stop copying if next motion trigger sooner than this
+    ignore_start_s = 2          #seconds don't search for motion in beginning of input file
+    ignore_end_s = 2            #seconds don't search for motion at end of input file
+    generate_output_file = True #set to False if you only want to read logs
+    delete_input_file = False   #DANGEROUS, use only if you have COPIES of input files
+
+    #out file parameters
+    out_prefix = ''             #begin output filename with this
+    out_from_in_start = 2       #substring of input file name. Large number copies entire filename
+    out_from_in_end = 0         #substring of end of input file name
+    out_random_letters = 1      #amount of random letters to add to output files. Same letters for all.
+    out_counter_digits = 2      #least digits to counter. 2 means _01, _02, _03 etc
+    out_lower_case = True       #force lower case filename
+    out_delimiter = ''          #e.g. '-' to make filename more readable
+
+    #cmd window log
+    print_scores = False        #whether to print frame info (including scene scores)
+    ffmpeg_loglevel = 31        #see https://ffmpeg.org/ffmpeg.html#Generic-options
+
+    #advanced filter parameters
+    step_len_f = 20             #compare every n frame
+    min_threshold_score = 0.0095#default threshold. a score above indicates motion
+    test_duration_s = 7         #seek for a ('motionless'ish') segment this long. threshold automatically adjusts up if necessary (and possible)
+    max_threshold_score = 0.04
+    segments_smooth = 0         #assign median score from n segments before and after to smooth out scores
+    segments_to_start = 2       #this many segments in a row above threshold triggers motion start
+    segments_to_end = 10        #this many segments in a row below threshold triggers motion end
+
+    #file checking
+    ffmpeg_check_file_args_pre = ['ffmpeg', '-v', 'debug', '-threads', '8', '-nostats', '-i']
+    ffmpeg_check_file_args_post = ['-f', 'null']
+
     def __init__(self):
         self._input_file = ''
 
@@ -80,6 +105,19 @@ class Detector:
         f = Path(input_)
         if f.exists() and not f.is_file():
             raise FileNotFoundError(f'Invalid file: {input_}')
+
+        # Use ffmpeg for checking that this file is a video file
+        # TODO Figure out why this interrupts other calls
+        # args = self.ffmpeg_check_file_args_pre
+        # args.extend([f'{input_}'])
+        # args.extend(self.ffmpeg_check_file_args_post)
+
+        # process = subprocess.Popen(
+        #     args, stdout=subprocess.PIPE, universal_newlines=True)
+        # process.wait()
+
+        # If we exit cleanly we'll set the file
+        # self._input_file = input_
 
         name_parts = f.name.split('.')
         if len(name_parts) == 2 and name_parts[1] in FILE_FORMATS:
@@ -94,6 +132,7 @@ class Detector:
         #get scenescores from ffmpeg
         # ffmpeg's scene change detection algo:
         # https://www.luckydinosaur.com/u/ffmpeg-scene-change-detector
+
         # writes to a txt file, parse it into lists, then delete file
         randint = random.randint(10000,99999)
         temp_file = "temp-scenescores-" + str(randint) + ".txt"
@@ -102,11 +141,11 @@ class Detector:
             os.remove(temp_file)
 
         command = "ffmpeg -loglevel " \
-            + str(ffmpeg_loglevel) \
+            + str(self.ffmpeg_loglevel) \
             + " -i \"" \
             + self.input_file \
             + "\" -vf select='not(mod(n\," \
-            + str(step_len_f) \
+            + str(self.step_len_f) \
             + "))',select='gte(scene,0)',metadata=print:file=" \
             + temp_file \
             + " -an -f null -"
@@ -149,10 +188,10 @@ class Detector:
         for x in f:
             f_median_score.append(
                 statistics.median(
-                    f_scene_score[max(0,x-segments_smooth):x+segments_smooth+1]))
+                    f_scene_score[max(0, x-self.segments_smooth): x + self.segments_smooth+1]))
         
         #try to increase threshold if no motionless period found 
-        file_threshold_score = min_threshold_score
+        file_threshold_score = self.min_threshold_score
 
         while True:
             longest_motionless_s = 0
@@ -165,10 +204,10 @@ class Detector:
                     if longest_motionless_s < run_s:
                         longest_motionless_s = run_s
                     last_change_s = f_pts_time[x]
-            if longest_motionless_s <= test_duration_s:
-                file_threshold_score += min_threshold_score
-                if file_threshold_score > max_threshold_score:
-                    file_threshold_score = min_threshold_score
+            if longest_motionless_s <= self.test_duration_s:
+                file_threshold_score += self.min_threshold_score
+                if file_threshold_score > self.max_threshold_score:
+                    file_threshold_score = self.min_threshold_score
                     break
             else:
                 break
@@ -183,21 +222,21 @@ class Detector:
         
         #frame's TRIGGER score [-1,0,+1]
         f_trigger = list()
-        x_max = len(f) - max(segments_to_start,segments_to_end)
+        x_max = len(f) - max(self.segments_to_start, self.segments_to_end)
         for x in f:
             if x >= x_max:
                 f_trigger.append(0)
                 continue
             run_above = 0
-            for y in range(segments_to_start):
-                if f_median_score[x+y] > file_threshold_score:
+            for y in range(self.segments_to_start):
+                if f_median_score[x + y] > file_threshold_score:
                     run_above += 1
-            if run_above == segments_to_start:
+            if run_above == self.segments_to_start:
                 f_trigger.append(1)
             else:
                 run_above = 0
-                for y in range(segments_to_end):
-                    if f_median_score[x+y] > file_threshold_score:
+                for y in range(self.segments_to_end):
+                    if f_median_score[x + y] > file_threshold_score:
                         run_above += 1
                 if run_above == 0:
                     f_trigger.append(-1)
@@ -207,12 +246,12 @@ class Detector:
         #based on trigger scores, select "smart" COPY start and end points [-1,0,+1]
         f_copy = list()
         is_copying = 0
-        end_time_s = f_pts_time[len(f)-1]
+        end_time_s = f_pts_time[len(f) - 1]
         for x in f:
             f_copy.append(0)
-            if f_pts_time[x] < ignore_start_s:
+            if f_pts_time[x] < self.ignore_start_s:
                 continue
-            if x >= x_max or f_pts_time[x] > end_time_s - ignore_end_s:
+            if x >= x_max or f_pts_time[x] > end_time_s - self.ignore_end_s:
                 if is_copying == 1:
                     #copy_end_s.append(f_pts_time[x])
                     f_copy[x] = -1
@@ -220,7 +259,7 @@ class Detector:
 
             #start copy?
             if is_copying == 0:
-                if f_pts_time[x] > end_time_s - ignore_end_s: #near end, don't make new starting point
+                if f_pts_time[x] > end_time_s - self.ignore_end_s: #near end, don't make new starting point
                     continue
                 if f_trigger[x] == 1:
                     #copy_start_s.append(f_pts_time[x])
@@ -240,7 +279,7 @@ class Detector:
                         if f_trigger[y] == 1:
                             can_end = 0
                             break
-                        if f_pts_time[y] - f_pts_time[x] > min_copy_break_s:
+                        if f_pts_time[y] - f_pts_time[x] > self.min_copy_break_s:
                             break
                     if can_end == 1:
                         #copy_end_s.append(f_pts_time[x])
@@ -259,15 +298,16 @@ class Detector:
 
         #adjust start and end times
         for x in range(len(copy_start_s)):
-            copy_start_s[x] = max(copy_start_s[x] - before_s, 0)
-            copy_end_s[x] = min(copy_end_s[x] + after_s, end_time_s)
+            copy_start_s[x] = max(copy_start_s[x] - self.before_s, 0)
+            copy_end_s[x] = min(copy_end_s[x] + self.after_s, end_time_s)
 
         #print output values
-        if print_scores:
+        if self.print_scores:
             print('*')
             print("Frame;Time;Score;Median;Change;Trigger;Copy")
             for x in f:
-                print(str(f[x]) + ";" + '%.4f'%(f_pts_time[x]) + ";" + '%.4f'%(f_scene_score[x]) + ";" + '%.4f'%(f_median_score[x]) + ";" + str(f_change[x]) + ";" + str(f_trigger[x]) + ';' + str(f_copy[x]))
+                print(str(f[x]) + ";" + '%.4f' % (f_pts_time[x]) + ";" + '%.4f' % (f_scene_score[x]) + ";" + '%.4f'%(f_median_score[x]) + ";" + str(f_change[x]) + ";" + str(f_trigger[x]) + ';' + str(f_copy[x]))
+            
             print(self.input_file)
 
         print("Threshold: " + str(file_threshold_score))
@@ -276,7 +316,7 @@ class Detector:
         if len(copy_start_s) > 0:
             print('Nr;Start;End;Duration')	
             for x in range(len(copy_start_s)):
-                print(str(x+1) + ';' + '%.2f'%(copy_start_s[x]) + ';' + '%.2f'%(copy_end_s[x]) + ';' + '%.2f'%(copy_end_s[x] - copy_start_s[x]))
+                print(str(x+1) + ';' + '%.2f' % (copy_start_s[x]) + ';' + '%.2f' % (copy_end_s[x]) + ';' + '%.2f'%(copy_end_s[x] - copy_start_s[x]))
 
         #copy each motion segment as new file
         # stream is copied; no re-encoding or loss in quality
@@ -285,31 +325,32 @@ class Detector:
 
         #prepare random characters in output file name
         out_random = ''
-        for i in range(out_random_letters):
+        for i in range(self.out_random_letters):
             out_random += random.choice("abcdefghijkmnopqrstuvwxyz")
 
         for x in range(len(copy_start_s)):
             while True:
                 file_num += 1
                 out_filename = ''
-                if out_prefix != '':
-                    out_filename += out_prefix + out_delimiter
-                if out_from_in_start > 0:
-                    out_filename += in_filename[:out_from_in_start] + out_delimiter
-                if out_from_in_end > 0:
-                    out_filename += in_filename[-out_from_in_end:] + out_delimiter
-                if out_random_letters > 0:
-                    out_filename += out_random + out_delimiter
-                out_filename += str(file_num).zfill(out_counter_digits)
+                if self.out_prefix != '':
+                    out_filename += self.out_prefix + self.out_delimiter
+                if self.out_from_in_start > 0:
+                    out_filename += in_filename[:self.out_from_in_start] + self.out_delimiter
+                if self.out_from_in_end > 0:
+                    out_filename += in_filename[-self.out_from_in_end:] + self.out_delimiter
+                if self.out_random_letters > 0:
+                    out_filename += out_random + self.out_delimiter
+                out_filename += str(file_num).zfill(self.out_counter_digits)
                 out_filename += file_extension
-                if out_lower_case:
+                if self.out_lower_case:
                     out_filename = out_filename.lower()
                 if os.path.isfile(out_filename) == False:
                     break
+
             clip_length = copy_end_s[x] - copy_start_s[x]
 
             command = 'ffmpeg -loglevel ' \
-                + str(ffmpeg_loglevel) \
+                + str(self.ffmpeg_loglevel) \
                 + ' -ss ' \
                 + '%.4f' % (copy_start_s[x]) \
                 + ' -i \"' \
@@ -321,11 +362,11 @@ class Detector:
 
             print('Run command: ' + command)
 
-            if generate_output_file:
+            if self.generate_output_file:
                 os.system(command)
         
         #delete input file?
-        if delete_input_file:
+        if self.delete_input_file:
             os.remove(self.input_file)
         
         #print time it took to process file
@@ -333,7 +374,7 @@ class Detector:
         process_s = t1-t0
         times_faster = end_time_s / process_s
         print('File processed in '
-            + '%.1f'%(process_s)
+            + '%.1f' % (process_s)
             + 's ('
-            + '%.1f'%(times_faster)
+            + '%.1f' % (times_faster)
             + 'x)')
