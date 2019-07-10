10dd221375908d965a52102d639d918cf81dd584
Holden.B <holden.bab@outlook.com>
2019-07-10 00:06:38 -0500

    Added detector code for running the ssd over frames of a video

--- pycvss/sample_files.py
+++ pycvss/sample_files.py
@@ -4,10 +4,12 @@ import os
 ######################################################################################
 # Data directory full path
 DATA_DIR = os.path.join(os.path.dirname(os.path.realpath(os.path.dirname(__file__))), 'data')
+
 # Sample video data directory
 SAMPLE_VIDEO_DIR = os.path.join(DATA_DIR, 'sample_video')
 
-# Sample video full path
+# Sample videos
 SAMPLE_VIDEO_PPRESS = os.path.join(SAMPLE_VIDEO_DIR, 'ppress_1080p_16-9_23-79fps.mp4')
 SAMPLE_VIDEO_DOGS = os.path.join(SAMPLE_VIDEO_DIR, 'dogs_1080p_16-9_25-fps.mp4')
 SAMPLE_VIDEO_HD_MOV = os.path.join(SAMPLE_VIDEO_DIR, 'Panasonic_HDC_TM_700_P_50i.mov')
+SAMPLE_VIDEO_RUNNING_DOG_HD = os.path.join(SAMPLE_VIDEO_DIR, 'running_dog_hd.mp4')
