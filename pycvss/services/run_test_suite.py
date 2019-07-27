b38f60a4181a3c37d0e6bc1f51475ed2e004e3d4
Holden.B <holden.bab@outlook.com>
2019-07-27 13:09:40 -0500

    Better format test suite service

--- pycvss/services/run_test_suite.py
+++ pycvss/services/run_test_suite.py
@@ -3,7 +3,7 @@ import pycvss.sample_files as sample_files
 import pycvss.ffmpeg.process_args as args
 
 
-######################################################################################
+###############################################################################
 # Keys
 OUTPUT_MP4 = 'mp4'
 OUTPUT_MKV = 'mkv'
@@ -20,7 +20,7 @@ OUTPUT_FILES = {
 }
 
 
-######################################################################################
+###############################################################################
 def print_sep() -> None:
     """Prints a seperator string of 100 characters"""
     print('-' * 100)
@@ -32,43 +32,61 @@ def _format_output(str_: str) -> None:
     print_sep()
 
 
-######################################################################################
+###############################################################################
 if __name__ == "__main__":
     # Test suite setup
     tests = [
         benchmark.BTest(
             OUTPUT_NAME_GRAYSCALE,
             lambda: args.grayscale_conversion_args(
-                sample_files.SAMPLE_VIDEO_PPRESS, OUTPUT_FILES[OUTPUT_MP4]),
-            input_file_=sample_files.SAMPLE_VIDEO_PPRESS, output_file_=OUTPUT_FILES[OUTPUT_MP4]
+                sample_files.SAMPLE_VIDEO_PPRESS,
+                OUTPUT_FILES[OUTPUT_MP4]),
+            input_file_=sample_files.SAMPLE_VIDEO_PPRESS,
+            output_file_=OUTPUT_FILES[OUTPUT_MP4]
         ),
 
         benchmark.BTest(
             OUTPUT_NAME_SCALE_VID,
             lambda: args.scale_video_args(
-                sample_files.SAMPLE_VIDEO_PPRESS, OUTPUT_FILES[OUTPUT_MP4], '1280:720'),
-            input_file_=sample_files.SAMPLE_VIDEO_PPRESS, output_file_=OUTPUT_FILES[OUTPUT_MP4]
+                sample_files.SAMPLE_VIDEO_PPRESS,
+                OUTPUT_FILES[OUTPUT_MP4],
+                '1280:720'),
+            input_file_=sample_files.SAMPLE_VIDEO_PPRESS,
+            output_file_=OUTPUT_FILES[OUTPUT_MP4]
         ),
 
         benchmark.BTest(
             OUTPUT_NAME_ENCODE_ADJUST,
             lambda: args.encode_and_adjust_args(
-                sample_files.SAMPLE_VIDEO_PPRESS, OUTPUT_FILES[OUTPUT_MKV], bitrate_=1, fps_=30, scale_=720),
-            input_file_=sample_files.SAMPLE_VIDEO_PPRESS, output_file_=OUTPUT_FILES[OUTPUT_MKV]
+                sample_files.SAMPLE_VIDEO_PPRESS,
+                OUTPUT_FILES[OUTPUT_MKV],
+                bitrate_=1,
+                fps_=30,
+                scale_=720),
+            input_file_=sample_files.SAMPLE_VIDEO_PPRESS,
+            output_file_=OUTPUT_FILES[OUTPUT_MKV]
         ),
 
         benchmark.BTest(
             OUTPUT_NAME_MOD_STREAM,
             lambda: args.modify_stream_args(
-                sample_files.SAMPLE_VIDEO_DOGS, OUTPUT_FILES[OUTPUT_MP4], '00:00:02', 5),
-            input_file_=sample_files.SAMPLE_VIDEO_PPRESS, output_file_=OUTPUT_FILES[OUTPUT_MP4]
+                sample_files.SAMPLE_VIDEO_DOGS,
+                OUTPUT_FILES[OUTPUT_MP4],
+                '00:00:02',
+                5),
+            input_file_=sample_files.SAMPLE_VIDEO_PPRESS,
+            output_file_=OUTPUT_FILES[OUTPUT_MP4]
         ),
 
         benchmark.BTest(
             OUTPUT_NAME_ENCODE_HW_ACCEL,
             lambda: args.hw_accel_encode(
-                sample_files.SAMPLE_VIDEO_PPRESS, OUTPUT_FILES[OUTPUT_MP4], 6, 10),
-            input_file_=sample_files.SAMPLE_VIDEO_HD_MOV, output_file_=OUTPUT_FILES[OUTPUT_MP4]
+                sample_files.SAMPLE_VIDEO_PPRESS,
+                OUTPUT_FILES[OUTPUT_MP4],
+                6,
+                10),
+            input_file_=sample_files.SAMPLE_VIDEO_HD_MOV,
+            output_file_=OUTPUT_FILES[OUTPUT_MP4]
         )
     ]
 
